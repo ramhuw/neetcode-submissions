05ae71396b7f7d062f4029f567f489a527373f78
ramhuw <45156158+ramhuw@users.noreply.github.com>
2026-05-02 16:49:09 -0400

    Add: binary-tree-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-1.rs
@@ -0,0 +1,30 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//     pub val: i32,
+//     pub left: Option<Rc<RefCell<TreeNode>>>,
+//     pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//     #[inline]
+//     pub fn new(val: i32) -> Self {
+//         TreeNode {
+//             val,
+//             left: None,
+//             right: None,
+//         }
+//     }
+// }
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        match root {
+            None => Vec::new(),
+            Some(r) => [Solution::inorder_traversal(r.borrow().left.clone()), vec![r.borrow().val], Solution::inorder_traversal(r.borrow().right.clone())].concat()
+        }
+    }
+}
